e206ed213fe23a24d131cb32199c8ee31dc675dd
Oren Ariel <oariel@Oren-MBP.local>
2019-04-30 09:55:00 -0700

    production port

--- slack/config-prod.js
+++ slack/config-prod.js
@@ -1,4 +1,4 @@
 process.env.clientId = "142758984291.622117040852";
 process.env.clientSecret = "21a1c2b254e668d956ad988c12c7d7de";
-process.env.port = 7001;
+process.env.port = 4001;
 process.env.mongodb_host = 'mongodb://0.0.0.0:27017';
