b61f81557e517069b7ca54f0534b0e3fa1a8dd90
root <root@baseimage.shaydev.capriza.com>
2019-04-30 23:32:51 +0000

    new bot under workspace-concierge

--- slack/config-prod.js
+++ slack/config-prod.js
@@ -1,4 +1,4 @@
-process.env.clientId = "142758984291.622117040852";
-process.env.clientSecret = "21a1c2b254e668d956ad988c12c7d7de";
-process.env.port = 4001;
+process.env.clientId = "622972935317.622478400980";
+process.env.clientSecret = "bef2ba2408575077eb9eb9ff52caba15";
+process.env.port = 2195;
 process.env.mongodb_host = 'mongodb://0.0.0.0:27017';
--- slack/teamList.js
+++ slack/teamList.js
@@ -1,6 +1,7 @@
 // Note that the default item [0] is returned if no matching team
 var team_list = [
-    {team: "*", endpoint: "http://0.0.0.0:2197"}
+    {team: "oren@arielfamily.com", endpoint: "http://0.0.0.0:2197"},
+    {team: "spaceiq.com", endpoint: "http://0.0.0.0:2197"}
 ];
 
 module.exports = team_list;
